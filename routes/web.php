e407d872890f7a920bcf46be5eefdd402a0f6590
Elvan Hidayat <elvanhd@gmail.com>
2023-07-09 02:20:16 +0700

    user login and logout

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,12 @@ Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 // Create New User
 Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
